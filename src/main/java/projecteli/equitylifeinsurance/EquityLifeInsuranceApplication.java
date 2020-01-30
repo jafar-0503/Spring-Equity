e13de03ed2c66557e0143ffe8ae7bc07084a63dc
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2020-01-30 10:21:58 +0700

    first commit

---
+++ src/main/java/projecteli/equitylifeinsurance/EquityLifeInsuranceApplication.java
@@ -0,0 +1,13 @@
+package projecteli.equitylifeinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquityLifeInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquityLifeInsuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projecteli/equitylifeinsurance/EquityLifeInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package projecteli.equitylifeinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquityLifeInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
